91bb8969037054346155330ec1081066fd72b598
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-02-19 15:07:19 +0800

    Added Recipe List, 1, 2 views and URLS

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,8 +1,12 @@
 # <appname>/urls.py
 from django.urls import path
-from .views import index 
+from .views import recipe_list, recipe_1, recipe_2, index
+
 urlpatterns = [
-    path('', index, name='index'),
+    path('',index, name='index'),
+    path('recipe/list',recipe_list,name='recipe list'),
+    path('recipe/1',recipe_1,name='recipe 1'),
+    path('recipe/2',recipe_2,name='recipe 2'),
 ]
 
 # This might be needed, depending on your Django version app_name = "<appname>"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Hello World! </h1>")
+    return HttpResponse("<h1>Welcome!</h1>")
+def recipe_list(request):
+    return HttpResponse("<h1>recipe list! </h1>")
+
+def recipe_1(request):
+    return HttpResponse("<h1>1 </h1>")
+
+def recipe_2(request):
+    return HttpResponse("<h1>2</h1>")
